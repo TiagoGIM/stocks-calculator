154883838202d36d339f8d6616bc33414ed724e8
TiagoGIM <herique.sa@gmail.com>
2023-08-15 16:39:39 -0300

    feat: symbolAverages to statusInvestSpreadsheet

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,23 @@
 import * as path from 'path';
 import * as Excel from 'exceljs';
 
-type Trade = {
-  date: string; // '19/08/2020'
-  operation_kind: string; // 'Compra'
-  symbol: string; //'TAEE11F',
-  quantity: number; //10,
-  value: number; //28.12,
-  amount: number; //281.2
-};
+enum OperationKind {
+  BUY = 'Compra',
+  SELL = 'Venda',
+}
+
+interface Trade {
+  date: string;
+  operation_kind: OperationKind;
+  symbol: string;
+  quantity: number;
+  value: number;
+  amount: number;
+}
 
 const workbook = new Excel.Workbook();
 
-const filePath = path.resolve(__dirname, 'assets/b3/negociacao-2021.xlsx');
+const filePath = path.resolve(__dirname, 'assets/b3/carteira-export.xlsx');
 
 const trades: Trade[] = [];
 
@@ -24,21 +29,68 @@ workbook.xlsx
       const rowData = row.values;
 
       if (Array.isArray(rowData)) {
+        const quantity = customParseInt(rowData[5]);
+        const value = customParseFloat(rowData[6]);
+
         const trade: Trade = {
           date: rowData[1] as string,
-          operation_kind: rowData[2] as string,
-          symbol: rowData[6] as string,
-          quantity: rowData[7] as number,
-          value: rowData[8] as number,
-          amount: rowData[9] as number,
+          operation_kind: rowData[4] === 'V' ? OperationKind.SELL : OperationKind.BUY,
+          symbol: rowData[3] as string,
+          quantity,
+          value,
+          amount: parseAmount(quantity, value),
         };
 
         trades.push(trade);
       }
     });
-
     console.log(trades);
+
+    const symbolMap = trades.reduce(
+      (acc, trade) => {
+        if (trade.operation_kind === OperationKind.BUY) {
+          if (!acc[trade.symbol]) {
+            acc[trade.symbol] = {
+              totalAmount: 0,
+              totalQuantity: 0,
+            };
+          }
+
+          acc[trade.symbol].totalAmount += trade.amount;
+          acc[trade.symbol].totalQuantity += trade.quantity;
+        }
+
+        return acc;
+      },
+      {} as Record<string, { totalAmount: number; totalQuantity: number }>,
+    );
+
+    const symbolAverages = Object.keys(symbolMap).map((symbol) => {
+      const { totalAmount, totalQuantity } = symbolMap[symbol];
+      const averagePrice = totalQuantity > 0 ? (totalAmount / totalQuantity).toFixed(2) : 0;
+
+      return {
+        symbol,
+        averagePrice,
+      };
+    });
+
+    console.log(symbolAverages);
   })
   .catch((error) => {
-    console.error('Erro ao ler o arquivo:', error);
+    console.error('Something wrong happen trying open the file', error);
   });
+
+function customParseInt(value: any): number {
+  const floatValue = customParseFloat(value);
+  return Math.round(floatValue);
+}
+
+function customParseFloat(value: any): number {
+  const floatValue = parseFloat(value.replace(',', '.'));
+  return Math.round(floatValue * 100) / 100;
+}
+
+function parseAmount(quantity: number, value: number): number {
+  return Math.round(quantity * value * 100) / 100;
+}
